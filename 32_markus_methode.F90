b99e6b7b6645c12859a85b11879d6c8bd22038a4
Andreas Hausoel <andreas.hausoel@physik.uni-wuerzburg.de>
2021-05-26 17:05:08 +0200

    a subroutine added

--- 32_markus_methode.F90
+++ 32_markus_methode.F90
@@ -2,10 +2,12 @@ module mymod
 
     type :: dTBath
         real :: x
+        real :: xsquare
     end type
 
     type :: zTBath
         complex :: x
+        complex :: xsquare
     end type
 
     type :: bath
@@ -18,30 +20,58 @@ contains
     type(bath) function generate_bath(ch) result(this)
         logical :: ch
 
-        if(ch)then
+        if(.not.ch)then
             allocate(this%dBath)
             write(*,*) "alloc real"
+            this%dBath%x = 2.0
         else
             allocate(this%zBath)
             write(*,*) "alloc compl"
+            this%zBath%x = (2.0, 0.0)
         endif
 
     end function
 
 end module
 
+module mymoves
+    use mymod
+
+contains
+
+    subroutine square(mybath, ch)
+        type(bath) :: mybath
+        logical :: ch
+
+        if(ch)then
+            mybath%zbath%xsquare = mybath%zbath%x**2
+        else
+            mybath%dbath%xsquare = mybath%dbath%x**2
+        endif
+
+    end subroutine
+
+end module
+
 program test
     use mymod
+    use mymoves
     implicit none
 
     type(bath) :: mybath
     logical :: ch
-    ch = .true.
     ch = .false.
 
     mybath = generate_bath(ch)
 
-    ! now the real functions use mybath%dbath%x 
-    ! and the complex ones mybath%zbath%x
+    ! Now the real functions use mybath%dbath%x 
+    ! and the complex ones mybath%zbath%x.
+    ! Is this correct?
+
+    call square(mybath, ch)
+    write(*,*) "mybath%dbath%xsquare", mybath%dbath%xsquare
+
+    ! I don't see, how here generic subroutines 
+    ! dsquare and zsquare could be called...
 
 end program
